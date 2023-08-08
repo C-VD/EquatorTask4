e4dfd3a7750a1e8ed4cb1a33d78560e06abe498d
C-VD <vldr@disroot.org>
2023-08-08 11:32:52 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // в исходном файле записан массив вещественных чисел:
+        File file = getFileFromUser();
+        Scanner sc;
+        try {
+            sc = new Scanner(file);
+        }
+        catch(Exception e) {
+            System.out.println("Не удалось прочитать файл");
+            return;
+        }
+            // в первой строчке целое число n,
+            int n = sc.nextInt();
+        // во второй строчке n вещественных чисел
+        sc.nextLine();
+;        double[] arr = new double[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextDouble();
+        }
+
+        // Определить медианное значение
+        double med = arr[n/2];
+        System.out.println("медианное значение: " + med);
+
+    }
+
+    static File getFileFromUser(){
+        System.out.println("Имя файла: ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        return new File(name);
+    }
+
+}
